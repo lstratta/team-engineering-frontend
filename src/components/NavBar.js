be5b873174e4a3d55c5079e8bf9eb994a26e4ba3
Eugene590 <85878942+Eugene590@users.noreply.github.com>
2021-12-15 14:30:32 +0000

    NavBar(with errors)
    Co-authored-by: niconealon <niconealon@users.noreply.github.com>
    Co-authored-by: AmeleyKA <AmeleyKA@users.noreply.github.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import '../css/header.css'
+// import { Link } from "react-router-dom";
 
 
-function NavBar() {
+export default function NavBar() {
     return (
         <div>
-            <nav>
-                <h1 class="Nav"></h1>
-                <div>
-                    <ul className="navitems">
-                        {/* <li>
-                            <Link to="/"></Link>
-                        </li> */}
-
-                        <li>
-                            <Link to="/Profile">Profile</Link>
-                        </li>
-
-                    </ul>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/"><img src="./assets/df-logo.png" alt="Digital Futures Logo" width="50px" /></a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Menu
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="/">Action</a></li>
+                                    <li><a class="dropdown-item" href="/">Another action</a></li>
+                                    <li><hr class="dropdown-divider" ></hr></li>
+                                    <li><a class="dropdown-item" href="/">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
     )
 }
-
-export default NavBar;
