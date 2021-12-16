66b33b54f3920031703d9e37475cb82b144ce56f
Eugene590 <85878942+Eugene590@users.noreply.github.com>
2021-12-16 21:03:15 +0000

    NavBar profile and home page link routed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,12 +9,12 @@ export default class NavBar extends Component {
             <div className='header2'>
                 <Navbar expand="lg">
                     <Container>
-                        <Navbar.Brand href="#home"><img src="./assets/df-logo.png" alt="Digital Futures Logo" width="50px" /></Navbar.Brand>
+                        <Navbar.Brand href="/"><img src="./assets/df-logo.png" alt="Digital Futures Logo" width="50px" /></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home"><img src="./assets/df-logo.png" alt="Digital Futures Logo" width="50px" /></Nav.Link>
-                                <Nav.Link href="#link">Profile</Nav.Link>
+                                <Nav.Link href="#home"></Nav.Link>
+                                <Nav.Link href="/EditProfile">Edit Profile</Nav.Link>
                                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
