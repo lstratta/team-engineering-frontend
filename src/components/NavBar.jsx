ddabc7fdc55d9a9fd40fd8c101d1fde6e9f16487
niconealon <nico.nealon1999@gmail.com>
2022-01-05 15:05:57 +0000

    Background image and css for navbar
    Co-authored-by: Eugene590 <Eugene590@users.noreply.github.com>
    Co-authored-by: AmeleyKA <AmeleyKA@users.noreply.github.com>

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 import '../css/header.css'
-import { renderMatches } from 'react-router-dom'
+import { renderMatches, Link } from 'react-router-dom'
+
 
 
 export default class NavBar extends Component {
@@ -14,14 +15,14 @@ export default class NavBar extends Component {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home"></Nav.Link>
-                                <Nav.Link href="/EditProfile"></Nav.Link>
-                                 <NavDropdown title="Menu" id="basic-nav-dropdown">
-                                    <NavDropdown.Item href="/EditProfile">Edit Profile</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2"></NavDropdown.Item>
+                                {/* <Nav.Link href="#home"></Nav.Link> */}
+                                {/* <Nav.Link href="/graduate/:id/edit"></Nav.Link> */}
+                                <NavDropdown className='dropdown' title="Menu" id="basic-nav-dropdown">
+                                    <NavDropdown.Item><Link className="menu-edit" to="/graduate/:id/edit">Edit Profile </Link></NavDropdown.Item>
+                                    {/* <NavDropdown.Item href="#action/3.2"></NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.3"></NavDropdown.Item>
                                     <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.4"></NavDropdown.Item> */}
                                 </NavDropdown>
                             </Nav>
                         </Navbar.Collapse>
